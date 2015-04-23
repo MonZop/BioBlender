ebb9f97ba108e9f994755cf2b5658e1964ae4d74
Dealga McArdle <tetha.z@gmail.com>
2015-04-23 22:00:40 +0200

    prelim code wipe

--- __init__.py
+++ __init__.py
@@ -1,61 +1,42 @@
-import bpy
-from bpy import *
-from urllib.parse import urlencode
-from urllib.request import *
-from html.parser import *
-from smtplib import *
-from email.mime.text import MIMEText
-import time, platform, os
-import codecs
-import bpy.path
-from bpy.path import abspath
-import base64
-import mathutils
-from mathutils import *
-from math import *
-import pickle
-import shutil
-import subprocess
-import sys
-
-
-
 bl_info = {
-    "name" : "BioBlender 2.0",
-    "author" : "SciVis, IFC-CNR",
-    "version" : (2,0),
-    "blender" : (2,6,8,0),
-    "location" : "Properties Window > Scene",
-    "description" : "BioBlender 2.0",
-    "warning" : "",
-    "wiki_url" : "",
-    "tracker_url" : "http://bioblender.eu/?page_id=665",
-    "category" : "Add Mesh"
+    "name": "BioBlender 2.5",
+    "author": "SciVis, IFC-CNR, et all",
+    "version": (2, 5),
+    "blender": (2, 7, 4),
+    "location": "Properties Window > Scene",
+    "description": "BioBlender 2.5 - rewrite of 2.0",
+    "warning": "",
+    "wiki_url": "",
+    "tracker_url": "http://bioblender.eu/?page_id=665",
+    "category": "Add Mesh"
 }
 
+'''
+the following if/else exists only to deal with
+F8 reloads, handy for development, and essential
+for co-existing on a Blender install where F8 is
+used frequently. Not all kinds of code changes can
+be dealt with by this reload strategy, in that case
+the only path is to close/reopen Blender.
+
+'''
+
+if 'bpy' in globals():
+    print(__package__, 'detected reload event')
+    if 'bioblender_main' in globals():
+        import imp
+        imp.reload(bioblender_main)
+        imp.reload(file_append_utils)
+        print('reloaded modules')
+else:
+    import bpy
+    from . import bioblender_main
+    from . import file_append_utils
 
 
-# ==================================================================================================================
-# ==================================================================================================================
-# ==================================================================================================================
-
-
-from .BioBlender2 import *
-
-
-# ==================================================================================================================
-# ==================================================================================================================
-# ==================================================================================================================
-
-
-
-# ===== REGISTER = START ================================================
 def register():
-    bpy.utils.register_module(__name__)  # @UndefinedVariable
+    bpy.utils.register_module(__name__)
+
 
 def unregister():
-    bpy.utils.unregister_module(__name__)  # @UndefinedVariable
-
-if __name__ == "__main__":
-    register()
-# ===== REGISTER = END ================================================
+    bpy.utils.unregister_module(__name__)
