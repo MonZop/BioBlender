e1183d4f0fe141e13ce3d07875b211d657f74ffe
Dealga McArdle <tetha.z@gmail.com>
2015-05-12 11:48:25 +0200

    added initial new file commit

---
+++ func_mlp_from_dxgrid.py
@@ -0,0 +1,12 @@
+# func_mlp_from_dxgrid.py
+import numpy as np
+
+
+def get_3dgrid_from_dx(filepath):
+    '''
+    This is a basic implementation of a dx grid reader which is intended only
+    to read the .dx produced by pyMLP.py at the request of BIoBlender25. I'm
+    making heavy assumptions about the consistency of these .dx files
+    '''
+
+    ...
